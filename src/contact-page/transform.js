319a220ff363081a7faba6809b9b77e4dc6c3fca
dgasch512 <gaschdav208@gmail.com>
2019-10-05 13:24:59 -0500

    contact mobile page complete with animations

---
+++ src/contact-page/transform.js
@@ -0,0 +1,12 @@
+$(".address").click(function() {
+  $('.address').toggleClass('transform-active');
+});
+$(".phone").click(function() {
+  $('.phone').toggleClass('transform-active');
+});
+$(".email").click(function() {
+  $('.email').toggleClass('transform-active');
+});
+$(".git").click(function() {
+  $('.git').toggleClass('transform-active');
+});
