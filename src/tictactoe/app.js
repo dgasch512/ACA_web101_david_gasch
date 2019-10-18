33d4ee7c52a21a904a688cdd3e7f99184727397c
dgasch512 <gaschdav208@gmail.com>
2019-10-18 14:33:40 -0500

    tic tac toe built following a tutorial on youtube

--- src/tictactoe/app.js
+++ src/tictactoe/app.js
@@ -1,20 +1,3 @@
-//  console.log('Helllloooooo')
-// // find location of click and add x or o
-// function addGamePiece(selectedElement) {
-//   // create new element
-//   let newElement = document.createElement('h1')
-//   // add text to element
-//   newElement.innerHTML = 'x'
-//   // add element with text to selectedElement on page
-//   selectedElement.appendChild(newElement)
-//   // tell me what I did
-//   console.log('show clicked div', selectedElement)
-// }
-
-// determine player1 or player2
-// keep score
-// find a winner via 3 in a row 
-
 
 var N_SIZE = 3,
   EMPTY = '&nbsp;',
